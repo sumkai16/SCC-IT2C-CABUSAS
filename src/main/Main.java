7df019985118dac801bfcd9c3b910dca7c660fca
sumkai16 <axceelfelis03@gmail.com>
2024-08-19 19:19:26 +0800

    grades nga hasul

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+
+package main;
+
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Grades gr = new Grades();
+       
+        
+       
+        gr.addGrade();
+       
+
+    }
+    
+}
